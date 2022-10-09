6f4ffcb54de71b4f4ca34a870c1d4790ec8eb791
zhangjh <njhxzhangjihong@126.com>
2022-10-09 10:37:47 +0800

    update

--- 聚合翻译/bg.js
+++ 聚合翻译/bg.js
@@ -1,11 +1,47 @@
-chrome.contextMenus.create({
-    title: '聚合翻译：%s',
-    contexts: ['selection'],
-    onclick: function(info, tab) {
+let transIconHtml = `
+    <div id="float-icon" style="">
+        <div class="float-icon" style="background-image: url('./icon_19.png');" />
+    </div>
+`;
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.contextMenus.create({
+        id: 'translate',
+        title: '聚合翻译',
+        type: 'normal',
+        contexts: ['selection'],
+    });
+
+    chrome.contextMenus.onClicked.addListener(function(info, tab) {
+        console.log(info);
+        const selectionText = info.selectionText;
+        // 合适的长度弹出弹窗
+        if(selectionText.length < 250) {
+            console.log($("title").text());
+        }
+        console.log(selectionText);
+
+        $("body").append(transIconHtml);
+
+        alert(111);
         chrome.tabs.create({
-            url: 'https://translate.google.cn/#en/zh-CN/' + encodeURI(info.selectionText)
+            url: 'https://www.baidu.com'
         });
-    }
-}, function() {
-    console.log(chrome.extension.lastError);
-})
+    });
+});
+//
+// chrome.contextMenus.create({
+//     title: '聚合翻译：%s',
+//     type: 'normal',
+//     id: 'combinedTranslate',
+//     contexts: ['all'],
+//     onclick: function(info, tab) {
+//         // chrome.tabs.create({
+//         //     url: 'https://translate.google.cn/#en/zh-CN/' + encodeURI(info.selectionText)
+//         // });
+//         console.log(info);
+//         alert(11);
+//     }
+// }, function() {
+//     console.log(chrome.extension.lastError);
+// })
